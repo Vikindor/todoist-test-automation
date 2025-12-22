c057af70d26f5b43c4233c42845d1385ffed1309
Vikindor <vikindor@protonmail.com>
2025-12-22 16:54:44 +0300

    feat: add component

---
+++ web/src/test/java/io/github/vikindor/web/ui/components/ProjectModal.java
@@ -0,0 +1,23 @@
+package io.github.vikindor.web.ui.components;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class ProjectModal {
+
+    private final SelenideElement
+            modal = $("[data-testid='modal-overlay']"),
+            nameInput = modal.$("input[name='name']"),
+            submitButton = modal.$("button[type='submit']");
+
+    public ProjectModal setName(String projectName) {
+        nameInput.setValue(projectName);
+        return this;
+    }
+
+    public ProjectModal clickSubmitButton() {
+        submitButton.click();
+        return this;
+    }
+}
