f5b2aa3c0ffbc7788ee9fd30647754c55c269b77
Vikindor <vikindor@proton.me>
2025-12-22 13:29:34 +0300

    feat: add tests components

---
+++ web/src/test/java/io/github/vikindor/web/ui/components/DeleteTaskModal.java
@@ -0,0 +1,15 @@
+package io.github.vikindor.web.ui.components;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+
+public class DeleteTaskModal {
+
+    private final SelenideElement modal = $("[data-testid='modal-overlay']");
+
+    public void clickDelete() {
+        modal.$$("button").findBy(text("Delete")).click();
+    }
+}
