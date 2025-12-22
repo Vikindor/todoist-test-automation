ca1d8f3c510d9597172103f2ff248d88c5615212
Vikindor <vikindor@protonmail.com>
2025-12-22 17:48:08 +0300

    chore: remove unused imports

--- web/src/test/java/io/github/vikindor/web/tests/ProjectsTests.java
+++ web/src/test/java/io/github/vikindor/web/tests/ProjectsTests.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.sleep;
 import static io.qameta.allure.Allure.step;
 
 @Epic("Web")
