bc063a6a3c29541b58fec3f26befea2765d60092
Vikindor <vikindor@proton.me>
2025-12-23 17:04:37 +0300

    feat: update PO and add shouldHaveProject, shouldNotHaveProject methods

--- web/src/test/java/io/github/vikindor/web/ui/pages/projects/ProjectsPage.java
+++ web/src/test/java/io/github/vikindor/web/ui/pages/projects/ProjectsPage.java
@@ -1,7 +1,10 @@
 package io.github.vikindor.web.ui.pages.projects;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -9,11 +12,25 @@ public class ProjectsPage {
 
     private final SelenideElement
             projectsOverview = $("[data-testid='personal-projects-overview']"),
+            projectsList = projectsOverview.$("ul[aria-label='Projects']"),
             addButton = projectsOverview.$("button[aria-haspopup='menu']"),
             addProjectAction = $(".reactist_menulist").$("[aria-label='Add project']");
 
+    private final ElementsCollection projectRow = projectsList.$$("[data-testid='project-row']");
+
     public ProjectsPage openPage() {
         open("/app/projects/active");
+        projectsOverview.shouldBe(visible);
+        return this;
+    }
+
+    public ProjectsPage shouldHaveProject(String projectName) {
+        projectRow.filterBy(text(projectName)).shouldHave(size(1));
+        return this;
+    }
+
+    public ProjectsPage shouldNotHaveProject(String projectName) {
+        projectRow.filterBy(text(projectName)).shouldHave(size(0));
         return this;
     }
 
