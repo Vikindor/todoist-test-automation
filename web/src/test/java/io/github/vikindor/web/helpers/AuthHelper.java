89251e909db2b785f2440f8b4e319861dd134622
Vikindor <vikindor@protonmail.com>
2025-12-25 13:00:11 +0300

    feat: add api auth methods

--- web/src/test/java/io/github/vikindor/web/helpers/AuthHelper.java
+++ web/src/test/java/io/github/vikindor/web/helpers/AuthHelper.java
@@ -1,17 +1,65 @@
 package io.github.vikindor.web.helpers;
 
-import io.github.vikindor.web.ui.pages.auth.LoginPage;
+import io.github.vikindor.web.api.TodoistAuthApi;
+import io.restassured.response.Response;
+import org.openqa.selenium.Cookie;
+
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class AuthHelper {
 
-    public static void login(String email, String password) {
-
-        LoginPage loginPage = new LoginPage();
-
-        loginPage.openPage();
-        loginPage
-                .setEmail(email)
-                .setPassword(password)
-                .clickLogInButton();
+    public static void apiLoginAndStabilize() {
+        Response response = TodoistAuthApi.login();
+        applyAuthCookies(response);
+        stabilizeAuthenticatedApp();
     }
+
+    private static void applyAuthCookies(Response loginResponse) {
+        open("/favicon.ico");
+
+        Map<String, String> cookies = loginResponse.getCookies();
+
+        cookies.forEach((name, value) -> {
+            if (isAuthCookie(name)) {
+                getWebDriver().manage().addCookie(
+                        new Cookie.Builder(name, value)
+                                .domain(".todoist.com")
+                                .path("/")
+                                .isSecure(true)
+                                .isHttpOnly(true)
+                                .build()
+                );
+            }
+        });
+    }
+
+    private static boolean isAuthCookie(String name) {
+        return name.equals("tduser") || name.equals("todoistd");
+    }
+
+    private static void stabilizeAuthenticatedApp() {
+        // Technical navigation to allow SPA auth context to finalize
+        open("/app/inbox");
+
+        // Todoist completes authentication asynchronously.
+        // No reliable DOM, cookie or network signal exists.
+        // Fixed delay acts as a synchronization barrier.
+        sleep(900);
+    }
+
+    // Legacy UI login (kept for fallback / experiments)
+//    public static void uiLogin(String email, String password) {
+//
+//        LoginPage loginPage = new LoginPage();
+//
+//        loginPage.openPage();
+//        loginPage
+//                .setEmail(email)
+//                .setPassword(password)
+//                .clickLogInButton();
+//    }
 }
