45907fa3c2472ee564bbb813a139e05f04045277
Vikindor <vikindor@protonmail.com>
2026-01-09 21:38:26 +0300

    feat: add new tests

--- mobile-android/src/test/java/io/github/vikindor/android/tests/LoginTests.java
+++ mobile-android/src/test/java/io/github/vikindor/android/tests/LoginTests.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Selenide;
 import io.github.vikindor.android.configs.ProjectConfig;
 import io.github.vikindor.android.helpers.Android;
 import io.github.vikindor.android.ui.screens.auth.GoogleScreen;
+import io.github.vikindor.android.ui.screens.auth.LoginScreen;
 import io.github.vikindor.android.ui.screens.auth.WelcomeScreen;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
@@ -30,9 +31,75 @@ public class LoginTests extends TestBase {
         return new GoogleScreen();
     }
 
+    LoginScreen loginScreen() {
+        return new LoginScreen();
+    }
+
     @Test
     @Tag("smoke")
-    @DisplayName("Open Google sign in")
+    @DisplayName("Email validation error is shown for invalid email")
+    void shouldShowEmailValidationError() {
+        String randomInvalidEmail = System.currentTimeMillis() + "@123";
+        String randomInvalidPassword = String.valueOf(System.currentTimeMillis()).substring(0, 8);
+
+        welcomeScreen()
+                .tapContinueWithEmailButton()
+                .tapLoginWithEmailButton();
+
+        step("Submit invalid email and password", () -> {
+            loginScreen()
+                    .setEmail(randomInvalidEmail)
+                    .setPassword(randomInvalidPassword)
+                    .tapLoginButton();
+        });
+
+        step("Verify email validation error", loginScreen()::shouldShowEmailValidationError);
+    }
+
+    @Test
+    @Tag("regression")
+    @DisplayName("Wrong credentials toast is shown for wrong password")
+    void shouldShowWrongEmailOrPasswordError() {
+        String randomInvalidPassword = String.valueOf(System.currentTimeMillis()).substring(0, 8);
+
+        welcomeScreen()
+                .tapContinueWithEmailButton()
+                .tapLoginWithEmailButton();
+
+        step("Submit valid email with invalid password", () -> {
+            loginScreen()
+                    .setEmail(config.todoistEmail())
+                    .setPassword(randomInvalidPassword)
+                    .tapLoginButton();
+        });
+
+        step("Verify wrong credentials error", () -> {
+            loginScreen().shouldShowCredentialsError();
+        });
+    }
+
+    @Test
+    @Tag("smoke")
+    @DisplayName("User can navigate to Forgot password from Login screen")
+    void shouldNavigateToForgotPasswordPage() {
+        String appPackage = config.appPackage();
+
+        welcomeScreen()
+                .tapContinueWithEmailButton()
+                .tapLoginWithEmailButton();
+
+        step("Navigate to Forgot your password", loginScreen()::tapForgotYourPasswordButton);
+
+        step("Verify external app opening attempt", () -> {
+            Selenide.Wait().until(driver ->
+                    !Android.getCurrentPackage().equals(appPackage)
+            );
+        });
+    }
+
+    @Test
+    @Tag("smoke")
+    @DisplayName("User can navigate to Google sign in from Welcome screen")
     void shouldOpenGoogleSignIn() {
         step("Open Google sign in", welcomeScreen()::tapContinueWithGoogleButton);
 
@@ -41,7 +108,7 @@ public class LoginTests extends TestBase {
 
     @Test
     @Tag("smoke")
-    @DisplayName("Open Facebook sign in")
+    @DisplayName("User can navigate to Facebook sign in from Welcome screen")
     void shouldOpenFacebookSignIn() {
         String appPackage = config.appPackage();
 
@@ -53,5 +120,4 @@ public class LoginTests extends TestBase {
             );
         });
     }
-
 }
