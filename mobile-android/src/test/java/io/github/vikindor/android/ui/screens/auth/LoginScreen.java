69a25cebaee82db65bd7e7cfdf962ad40ba3d10c
Vikindor <vikindor@protonmail.com>
2026-01-09 21:38:54 +0300

    feat: add PO for new tests

--- mobile-android/src/test/java/io/github/vikindor/android/ui/screens/auth/LoginScreen.java
+++ mobile-android/src/test/java/io/github/vikindor/android/ui/screens/auth/LoginScreen.java
@@ -1,7 +1,9 @@
 package io.github.vikindor.android.ui.screens.auth;
 
 import com.codeborne.selenide.SelenideElement;
+import io.github.vikindor.android.helpers.Android;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static io.appium.java_client.AppiumBy.androidUIAutomator;
 
@@ -10,7 +12,10 @@ public class LoginScreen {
     private final SelenideElement
             emailInput = $(androidUIAutomator("new UiSelector().resourceId(\"email\")")),
             passwordInput = $(androidUIAutomator("new UiSelector().resourceId(\"password\")")),
-            loginButton = $(androidUIAutomator("new UiSelector().resourceId(\"auth_button_tag\")"));
+            loginButton = $(androidUIAutomator("new UiSelector().resourceId(\"auth_button_tag\")")),
+            forgotYourPasswordButton =
+            $(androidUIAutomator("new UiSelector().className(\"android.widget.Button\").instance(1)")),
+            emailValidationError = $(androidUIAutomator("new UiSelector().text(\"Email is invalid\")"));
 
     public LoginScreen setEmail(String email) {
         emailInput.sendKeys(email);
@@ -24,6 +29,20 @@ public class LoginScreen {
 
     public LoginScreen tapLoginButton() {
         loginButton.click();
+        return this;
+    }
+
+    public void tapForgotYourPasswordButton() {
+        forgotYourPasswordButton.click();
+    }
+
+    public LoginScreen shouldShowEmailValidationError() {
+        emailValidationError.shouldBe(visible);
+        return this;
+    }
+
+    public LoginScreen shouldShowCredentialsError() {
+        Android.shouldHaveToast("Couldn't log in. Please check your credentials and try again.");
         return this;
     }
 }
