6c4843d95638477bcf3d0035ef3a7fc91a6519f8
Vikindor <vikindor@protonmail.com>
2026-01-09 15:21:47 +0300

    refactor: remove unused properties

--- api/src/test/java/io/github/vikindor/models/ErrorResponse.java
+++ api/src/test/java/io/github/vikindor/models/ErrorResponse.java
@@ -1,18 +1,8 @@
 package io.github.vikindor.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ErrorResponse {
     public String error;
-
-    @JsonProperty("error_code")
-    public int errorCode;
-
-    @JsonProperty("error_tag")
-    public String errorTag;
-
-    @JsonProperty("http_code")
-    public int httpCode;
 }
