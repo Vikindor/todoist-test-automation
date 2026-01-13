94d70322d8535cd4bd39e71608af36520f555ca2
Vikindor <vikindor@protonmail.com>
2026-01-13 23:31:52 +0300

    feat: add new page object and methods

--- web/src/test/java/io/github/vikindor/web/ui/components/QuickAddModal.java
+++ web/src/test/java/io/github/vikindor/web/ui/components/QuickAddModal.java
@@ -2,13 +2,16 @@ package io.github.vikindor.web.ui.components;
 
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class QuickAddModal {
 
     private final SelenideElement
             taskNameInput = $("[aria-label='Task name']"),
-            submitButton = $("[data-testid='task-editor-submit-button']");
+            submitButton = $("[data-testid='task-editor-submit-button']"),
+            taskNameLimitError = $("#a11y_task_name_over_limit");
 
     public QuickAddModal setTaskName(String taskName) {
         taskNameInput.setValue(taskName);
@@ -17,6 +20,16 @@ public class QuickAddModal {
 
     public QuickAddModal clickSubmitButton() {
         submitButton.click();
+        return this;
+    }
+
+    public QuickAddModal shouldShowCharacterLimitError() {
+        taskNameLimitError.shouldBe(visible);
+        return this;
+    }
+
+    public QuickAddModal shouldHaveDisabledSubmitButton() {
+        submitButton.shouldHave(attribute("aria-disabled", "true"));
         return this;
     }
 }
