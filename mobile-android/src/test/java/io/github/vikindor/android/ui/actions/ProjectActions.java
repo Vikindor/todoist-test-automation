fa2162546b8bb74f3c867ba0904e681f39fbcdab
Vikindor <vikindor@protonmail.com>
2026-01-14 00:30:30 +0300

    refactor: simplify cleanup logic to reduce execution time

--- mobile-android/src/test/java/io/github/vikindor/android/ui/actions/ProjectActions.java
+++ mobile-android/src/test/java/io/github/vikindor/android/ui/actions/ProjectActions.java
@@ -1,15 +1,15 @@
 package io.github.vikindor.android.ui.actions;
 
-import static io.github.vikindor.android.ui.screens.Screens.*;
+import static io.github.vikindor.android.ui.screens.Screens.projectScreen;
+import static io.github.vikindor.android.ui.screens.Screens.alertDialog;
 
 public class ProjectActions {
 
-    public static void projectCleanup(String projectName) {
+    public static void projectCleanup() {
         projectScreen()
                 .openProjectOptionsMenu()
                 .tapDelete();
+
         alertDialog().tapDelete();
-        navigationBar().tapBrowse();
-        browseScreen().shouldNotHaveProject(projectName);
     }
 }
