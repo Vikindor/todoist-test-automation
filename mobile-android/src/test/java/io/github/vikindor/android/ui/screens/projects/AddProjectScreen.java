a4c6342a8fe49704dc25631192a26ec0ff3cf1a3
Vikindor <vikindor@protonmail.com>
2026-01-14 15:16:49 +0300

    feat: add shouldShowNameRequiredError

--- mobile-android/src/test/java/io/github/vikindor/android/ui/screens/projects/AddProjectScreen.java
+++ mobile-android/src/test/java/io/github/vikindor/android/ui/screens/projects/AddProjectScreen.java
@@ -2,6 +2,7 @@ package io.github.vikindor.android.ui.screens.projects;
 
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static io.appium.java_client.AppiumBy.id;
 
@@ -9,7 +10,8 @@ public class AddProjectScreen {
 
     private final SelenideElement
             projectNameInput = $(id("com.todoist:id/name")),
-            doneButton = $(id("com.todoist:id/menu_form_submit"));
+            doneButton = $(id("com.todoist:id/menu_form_submit")),
+            projectNameError = $(id("com.todoist:id/textinput_error"));
 
     public AddProjectScreen setProjectName(String projectName) {
         projectNameInput.sendKeys(projectName);
@@ -18,6 +20,11 @@ public class AddProjectScreen {
 
     public AddProjectScreen tapDone() {
         doneButton.click();
+        return this;
+    }
+
+    public AddProjectScreen shouldShowNameRequiredError() {
+        projectNameError.shouldHave(text("Name is required."));
         return this;
     }
 }
