2bf1d431da23fa04b8d5a372b014b289c481a35e
Vikindor <vikindor@protonmail.com>
2026-01-26 19:08:44 +0300

    feat: add new and update keys for multiple accounts

--- web/src/test/java/io/github/vikindor/web/configs/ProjectConfig.java
+++ web/src/test/java/io/github/vikindor/web/configs/ProjectConfig.java
@@ -5,12 +5,15 @@ import org.aeonbits.owner.Config;
 @Config.Sources({"classpath:auth.properties"})
 public interface ProjectConfig extends Config {
 
-    @Key("todoist.email")
-    String todoistEmail();
+    @Key("todoist.accounts.count")
+    int todoistAccountsCount();
 
-    @Key("todoist.password")
-    String todoistPassword();
+    @Key("todoist.accounts[%d].email")
+    String todoistEmail(int accountIndex);
 
-    @Key("todoist.device_id")
-    String todoistDeviceId();
+    @Key("todoist.accounts[%d].password")
+    String todoistPassword(int accountIndex);
+
+    @Key("todoist.accounts[%d].device_id")
+    String todoistDeviceId(int accountIndex);
 }
