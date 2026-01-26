d9080bae67ac31b5f60bea2f169fa6d931f8a1a5
Vikindor <vikindor@protonmail.com>
2026-01-26 19:11:22 +0300

    feat: add thread-based account selection

--- web/src/test/java/io/github/vikindor/web/extensions/WithLoginExtension.java
+++ web/src/test/java/io/github/vikindor/web/extensions/WithLoginExtension.java
@@ -1,10 +1,18 @@
 package io.github.vikindor.web.extensions;
 
+import io.github.vikindor.web.configs.ProjectConfig;
+import io.github.vikindor.web.context.CurrentUser;
 import io.github.vikindor.web.helpers.AuthHelper;
+import io.github.vikindor.web.utils.AllureAttach;
+import org.aeonbits.owner.ConfigFactory;
+import org.jspecify.annotations.NonNull;
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
 import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
-public class WithLoginExtension implements BeforeTestExecutionCallback {
+public class WithLoginExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback {
+
+    private static final ProjectConfig config = ConfigFactory.create(ProjectConfig.class);
 
     @Override
     public void beforeTestExecution(ExtensionContext context) {
@@ -20,6 +28,20 @@ public class WithLoginExtension implements BeforeTestExecutionCallback {
             return;
         }
 
-        AuthHelper.apiLoginAndStabilize();
+        int accountsCount = config.todoistAccountsCount();
+        long threadId = Thread.currentThread().getId();
+        int accountIndex = (int) (threadId % accountsCount);
+        String email = config.todoistEmail(accountIndex);
+
+        AllureAttach.userSession(accountIndex, email);
+
+        CurrentUser.setAccountIndex(accountIndex);
+
+        AuthHelper.apiLoginAndStabilize(accountIndex);
+    }
+
+    @Override
+    public void afterTestExecution(@NonNull ExtensionContext context) {
+        CurrentUser.clearAccountIndex();
     }
 }
